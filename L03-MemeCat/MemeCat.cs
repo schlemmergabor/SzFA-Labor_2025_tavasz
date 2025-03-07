88aa4ceb2343eba5094804d2da1436cf45daf2a9
schlemmergabor <schlemmer.gabor@nik.uni-obuda.hu>
2025-03-07 08:46:32 +0100

    L03 - MemeCat - OK

---
+++ L03-MemeCat/MemeCat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L03_MemeCat
+{
+    internal class MemeCat : IComparable
+    {
+        // privát adattag, mező, belső változó
+        int age;
+        string name;
+
+        // Konstruktor, amely inicializálja az életkort és a nevet
+        public MemeCat(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+
+        // Az Array.Sort() metódus ezt használja az objektumok összehasonlítására
+        // A visszatérési érték:
+        // -1: ha az aktuális objektum (this) kisebb, mint az összehasonlított objektum
+        //  0: ha egyenlők
+        // +1: ha az aktuális objektum nagyobb, mint az összehasonlított objektum
+        public int CompareTo(object? obj)
+        {
+            // Az obj objektumot MemeCat típussá alakítjuk
+            MemeCat temp = obj as MemeCat;
+
+            // Ha nem sikerül a MemeCat-é alakítás, akkor ...
+            // (Itt, célszerű lenne kivételt dobni
+            // Később a félév során erről részletesebben lesz szó)
+            if (temp == null) return 0;
+
+            // Életkor (age) mezők szerinti összehasonlítás
+
+            // ha a this után jön a temp (obj)
+            if (this.age < temp.age) return -1;
+
+            // ha a temp (obj) után jön a this
+            if (this.age > temp.age) return 1;
+            
+            // ha megegyezik a sorrendjük
+            return 0;
+
+        }
+    }
+}
---
+++ L03-MemeCat/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace L03_MemeCat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tömb létrehozása néhány mém macskával tesztelés céljából
+
+            MemeCat[] cats = new MemeCat[]
+            {
+                // 
+                new MemeCat(8, "Nyan Cat"),
+                new MemeCat(10, "Grumpy Cat"),
+                new MemeCat(2, "Smudge Cat")
+            };
+
+            // Beépített rendezés az életkor alapján
+            Array.Sort(cats);
+
+            // Breakpoint ellenőrzéshez egy üres utasítás
+            ;
+        }
+    }
+}
