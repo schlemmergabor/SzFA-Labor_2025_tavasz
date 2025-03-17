e147962a072419047c4eae37455afee32a5c9642
schlemmergabor <schlemmer.gabor@nik.uni-obuda.hu>
2025-03-17 10:30:47 +0100

    L04 - OK

---
+++ L04-PrimeTool/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace L04_PrimeTool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // A program tesztelése a L04-Unit_Tests nevű projektben történik
+        }
+    }
+}
