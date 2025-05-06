2dd4915e275397cd5f82d80f30c1d7ab87b73362
schlemmergabor <schlemmer.gabor@nik.uni-obuda.hu>
2025-05-06 17:31:35 +0200

    L10 - Metszet-ig ok

---
+++ L10-Halmazok/IParticipant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L10_Halmazok
+{
+    public interface IParticipant : IComparable
+    {
+        // Property
+        public string Name { get; }
+    }
+
+}
---
+++ L10-Halmazok/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace L10_Halmazok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
